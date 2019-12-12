36537f05fedf2c7f9429c236609b351f187d2889
Aleksandr <aleksandr.v.dikarev@gmail.com>
2019-12-12 12:28:04 +0400

    Add files via upload

---
+++ STM32/main.c
@@ -0,0 +1,11 @@
+
+#include <main.h>
+
+int main(void)
+{
+	TAL_Init();
+	uMCP_PPNode_Init();
+	uMCP_PPNode_Run();
+
+	return 1;
+}
---
+++ STM32/main.h
@@ -0,0 +1,10 @@
+// (C) Aleksandr Dikarev, 2015
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <TAL_peripherials.h>
+#include <uMCP.h>
+
+
+#endif
